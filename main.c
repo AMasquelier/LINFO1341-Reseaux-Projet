28ea8bd338fc5771f6647dd8d17925f4062bf063
AMasquelier <antoine.masquelier@student.uclouvain.be>
2019-10-02 16:38:08 +0200

    a

--- main.c
+++ main.c
@@ -4,6 +4,42 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
+typename struct TRTP_packet
+{
+    int type = 0;
+	bool tr = false;
+	int window = 0;
+	bool L = false;
+	int Length = 0;
+} TRTP_packet;
+
+TRTP_packet read_TRTP_packet(uint32_t header)
+{
+    TRTP_packet packet;
+
+    bool bits[32];
+
+	for (int i = 31; i >= 0; i--) bits[31-i] = ((x >> i) & 1);
+
+
+    // Type
+	packet.type = bits[0] * 2 + bits[1];
+    // Truncated ?
+	packet.tr = bits[2];
+    // Window
+	for (int i = 0; i < 5; i++) packet.window = 2 * packet.window + bits[3 + i];
+    // L
+	packet.L = bits[8];
+    // Length
+	for (int i = 0; i < 7 + 8 * L; i++) packet.Length = 2 * packet.Length + bits[9 + i];
+    
+	if (packet.type != 1 && packet.tr) printf("Ignored"\n);
+
+	if (packet.type == 1 && packet.Length == 0 /* && ... */) printf("Transfer ended"\n);
+
+    return packet;
+}
+
 int create_socket(struct sockaddr_in6 *dest_addr, int dst_port)
 {
     if (dest_addr == NULL || dst_port <= 0)
