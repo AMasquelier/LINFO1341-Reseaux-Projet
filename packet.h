2061061d615a06b0c084f2eb7ec7a7fbce641e3e
AMasquelier <antoine.masquelier@student.uclouvain.be>
2019-10-03 17:29:15 +0200

    A

--- packet.h
+++ packet.h
@@ -6,27 +6,4 @@ typedef struct TRTP_packet
     uint32_t type : 2, tr : 1, window : 5, L : 1, length : 15, seqnum : 8;
 } TRTP_packet;
 
-TRTP_packet read_TRTP_packet(uint32_t header)
-{
-    TRTP_packet packet;
-
-    uint8_t bits[32];
-	for (int i = 31; i >= 0; i--) bits[31-i] = ((header >> i) & 1);
-
-    // Type
-	packet.type = bits[0] * 2 + bits[1];
-    // Truncated ?
-	packet.tr = bits[2];
-    // Window
-	for (int i = 0; i < 5; i++) packet.window = 2 * packet.window + bits[3 + i];
-    // L
-	packet.L = bits[8];
-    // Length
-	for (int i = 0; i < 7 + 8 * packet.L; i++) packet.length = 2 * packet.length + bits[9 + i];
-
-	if (packet.type != 1 && packet.tr) printf("Ignored\n");
-
-	if (packet.type == 1 && packet.length == 0 /* && ... */) printf("Transfer ended\n");
-
-    return packet;
-}
+TRTP_packet read_TRTP_packet(uint32_t header);
