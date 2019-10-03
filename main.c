fdb5c4890b56a7681974ebc664b9d8539c9b35e3
AMasquelier <antoine.masquelier@student.uclouvain.be>
2019-10-03 17:17:38 +0200

    a

--- main.c
+++ main.c
@@ -1,45 +1,11 @@
-#include <stdlib.h> /* EXIT_X */
-#include <stdio.h> /* fprintf */
-#include <unistd.h> /* getopt */
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
 #include <netdb.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
+#include "packet.h"
 
-/*typedef struct TRTP_packet
-{
-    int type = 0;
-	bool tr = false;
-	int window = 0;
-	bool L = false;
-	int Length = 0;
-} TRTP_packet;
-
-TRTP_packet read_TRTP_packet(uint32_t header)
-{
-    TRTP_packet packet;
-
-    bool bits[32];
-
-	for (int i = 31; i >= 0; i--) bits[31-i] = ((x >> i) & 1);
-
-
-    // Type
-	packet.type = bits[0] * 2 + bits[1];
-    // Truncated ?
-	packet.tr = bits[2];
-    // Window
-	for (int i = 0; i < 5; i++) packet.window = 2 * packet.window + bits[3 + i];
-    // L
-	packet.L = bits[8];
-    // Length
-	for (int i = 0; i < 7 + 8 * L; i++) packet.Length = 2 * packet.Length + bits[9 + i];
-
-	if (packet.type != 1 && packet.tr) printf("Ignored"\n);
-
-	if (packet.type == 1 && packet.Length == 0 /* && ... ) printf("Transfer ended"\n);
-
-    return packet;
-}*/
 
 const char *real_address(const char *address, struct sockaddr_in6 *addr)
 {
@@ -48,7 +14,7 @@ const char *real_address(const char *address, struct sockaddr_in6 *addr)
     struct addrinfo* res;
     int error;
 
-    /* resolve the domain name into a list of addresses */
+
     error = getaddrinfo(address, NULL, NULL, &result);
     if (error != 0) {
         if (error == EAI_SYSTEM) {
@@ -59,7 +25,6 @@ const char *real_address(const char *address, struct sockaddr_in6 *addr)
         exit(EXIT_FAILURE);
     }
 
-    /* loop over all returned results and do inverse lookup */
     for (res = result; res != NULL; res = res->ai_next) {
         char hostname[NI_MAXHOST];
         error = getnameinfo(res->ai_addr, res->ai_addrlen, hostname, NI_MAXHOST, NULL, 0, 0);
@@ -102,7 +67,7 @@ int create_socket(struct sockaddr_in6 *dest_addr, int dst_port)
 int main(int argc, char *argv[])
 {
     int client = 0;
-	int port = 1341;
+	int port = 12345;
 	int opt;
 	char *host = "::1";
 
